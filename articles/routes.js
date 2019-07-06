7bf44ab73b7d35b28c9d7287a85e5c7cb18b9006
Raul Novelo <rnovelo.cruz98@gmail.com>
2019-07-06 18:07:58 -0500

    chore: code style

--- articles/routes.js
+++ articles/routes.js
@@ -1,42 +1,43 @@
 const express = require('express');
 const router = express.Router();
-const { registerArticle }  = require('./controllers')
+const { registerArticle } = require('./controllers')
+const connection = require('../dao/connection')
+
+/* GET all articles. */
+router.get('/', async (req, res, next) => {
+  const { Article } = await connection()
+  res.status(200).send(Article.findAll());
+});
+
+/* GET one article. */
+router.get('/:uuid', async (req, res, next) => {
+  const { Article } = await connection()
+  const uuid = req.params.uuid
+  res.status(200).send(Article.findOne({
+    where: {
+      uuid
+    }
+  }));
+});
 
 /* POST articles listing. */
-router.post('/registerArticle', (req, res, next) => {
-    const article = req.body
-    registerArticle(article).then(article => {
-      res.status(200).send(article);
-    }, e => {
-      res.status(500).send(e);
-    })
-  });
+router.post('/', (req, res, next) => {
+  const article = req.body
+  registerArticle(article).then(article => {
+    res.status(200).send(article);
+  }, e => {
+    res.status(500).send('Internal Server Error');
+  })
+});
 
-  /* GET all articles. */
-router.get('/getArticles', async (req, res, next) => {
-    const { Article } = await connection()
-    console.log(Article)
-    res.status(200).send(Article.findAll());
-  });
-   /* GET articles by title */
-   router.get('/getArticlebyTitle:title', async (req, res, next) => {
-    const { Article } = await connection()
-    const title = req.params.title
-    res.status(200).send(Article.findOne({
-      where: {
-        title
-      }
-    }));
-  });
-  
-  /* GET one article. */
-  router.get('/getArticlebyUuid:uuid', async (req, res, next) => {
-    const { Article } = await connection()
-    const uuid = req.params.uuid
-    res.status(200).send(Article.findOne({
-      where: {
-        uuid
-      }
-    }));
-  });
-  module.exports = router;
+/* PUT articles listing. */
+router.put('/', (req, res, next) => {
+  const article = req.body
+  registerArticle(article).then(article => {
+    res.status(200).send(article);
+  }, e => {
+    res.status(500).send('Internal Server Error');
+  })
+});
+
+module.exports = router;
--- profiles/routes.js
+++ profiles/routes.js
@@ -2,12 +2,15 @@ const express = require('express');
 const router = express.Router();
 const { registerProfile }  = require('./controllers')
 
-router.post('/registerProfile', (req, res, next) => {
+/**
+ * POST Create new Author Profile
+ */
+router.post('/', (req, res, next) => {
   const profile = req.body
   registerProfile(profile).then(profile => {
     res.status(200).send(profile);
   }, e => {
-    res.status(500).send(e);
+    res.status(500).send('Internal Server Error');
   })
 });
 
