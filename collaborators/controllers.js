13ce55f4d45df572862fa460c0d4967eb970a339
Raul Novelo <rnovelo.cruz98@gmail.com>
2019-07-16 01:38:11 -0500

    explain createOrUpdate function with associations

--- collaborators/controllers.js
+++ collaborators/controllers.js
@@ -1,16 +1,16 @@
 const connection = require('../dao/connection')
 
 async function createOrUpdate(collaborator) {
-    const { Collaborator, Partner, Role } = await connection()
-    let cond, existingCollaborator, existingPartner, partner = null
-    console.log(collaborator)
-    if (collaborator.uuid) {
+    const { Collaborator, Partner, Role } = await connection() // modelos involucrados en el proceso
+    let cond, existingCollaborator, existingPartner, partner // inicializar banderas
+    
+    if (collaborator.uuid) { // existe entonces debemos buscar por uuid
         cond = {
             where: {
                 uuid: collaborator.uuid
             }
         }
-    } else {
+    } else { // es nuevo? checamos que no exista uno con mismo nombre y apellido
         cond = {
             where: {
                 name: collaborator.name,
@@ -18,30 +18,41 @@ async function createOrUpdate(collaborator) {
             }
         }
     }
-    existingPartner = await Partner.findOne({ where: collaborator.Adscription })
-    existingCollaborator = await Collaborator.findOne(cond)
+    existingPartner = await Partner.findOne({ where: collaborator.Adscription }) // la adscripcion debe venir el body como un objeto
+    existingCollaborator = await Collaborator.findOne(cond) // buscamos al colaborador
 
+    // si no existe lo creamos y lo asigno a mi variable de modelo Partner
     if (existingPartner)
         partner = existingPartner
     else
         partner = await Partner.create(collaborator.Adscription)
 
+    // si no existe lo creamos y lo asigno a mi variable de modelo Collaborator
     if (existingCollaborator)
         await Collaborator.update(collaborator, cond)
     else
         existingCollaborator = await Collaborator.create(collaborator)
 
+    // a cualquier de los dos le asignó su adscription
+    // el metodo setPartner() me lo brinda sequelize con la asociación
     existingCollaborator.setPartner(partner)
-    collaborator.Roles.forEach(async (role) => {
+
+    // por ultimo le toca a los roles
+    collaborator.Roles.forEach(async (role) => { // por cada role igual hay que preguntar, buscar y/o crear
         let new_role = await Role.findOne({ where: { name: role } })
         if (!new_role) {
             new_role = await Role.create({ name: role })
         }
-        await existingCollaborator.addRole(new_role)
+        await existingCollaborator.addRole(new_role) // de ultimo lo agrego a sus roles esto se va ala tabla CollaboratorRoles
     });
-    return existingCollaborator.dataValues
+    return existingCollaborator.dataValues // retorno el modelo se sequel pero solo el objeto que contiene los valores
 }
 
+/**
+ * Async function to return collaborators with roles
+ * waiting for get roles due to getRoles() is asyncronous
+ * @param {Array} collaborators 
+ */
 async function getCollaborators(collaborators) {
     for (let index = 0; index < collaborators.length; index++) {
         let coll = collaborators[index]
