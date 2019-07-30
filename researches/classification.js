8532f4180e6af9956cf1c0139821736881e04382
Raul Novelo <rnovelo.cruz98@gmail.com>
2019-07-29 19:44:49 -0500

    presentation => ponencia

--- researches/classification.js
+++ researches/classification.js
@@ -1,6 +1,6 @@
 'use strict'
 async function classification(researches) {
-    let t = ["thesis", "tesis"], pu = ["publication", "publicacion", "publicación"], pre = ["presentation", "presentacion", "presentación"]
+    let t = ["thesis", "tesis"], pu = ["publication", "publicacion", "publicación"], pre = ["presentation", "ponencia", "ponencia"]
     let theses = researches.filter(r => t.includes(r.type.toLowerCase()))
     let publications = researches.filter(r => pu.includes(r.type.toLowerCase()))
     let presentations = researches.filter(r => pre.includes(r.type.toLowerCase()))
